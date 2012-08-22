169b6b189398fdf4d5a05064b017bf72883e6733
goto100 <goto100@gmail.com>
2012-08-22 17:37:22 +0800

    developing

--- test/package.js
+++ test/package.js
@@ -3,14 +3,14 @@ var path = require('path');
 var pkg = require('../lib/opm/package');
 var package = new pkg.Package(path.join(__dirname, 'workspace/abc'));
 
-describe('compile', function() {
+describe('build', function() {
 	it('should loaded', function(done) {
 		package.load(function() {
 			done();
 		});
 	});
-	it('should compiled', function(done) {
-		package.compile('abc.js', function(err, content) {
+	it('should built', function(done) {
+		package.buildFile('abc.js', function(err, content) {
 			console.log(content, err);
 			done();
 		});
