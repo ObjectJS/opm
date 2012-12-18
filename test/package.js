b045a2d240f076d7ad1b0d0625d0dbb0593d1787
goto100 <goto100@gmail.com>
2012-12-18 15:56:31 +0800

    spm

--- test/package.js
+++ test/package.js
@@ -2,13 +2,33 @@ var assert = require('assert');
 var path = require('path');
 var opm = require('../lib/opm');
 
-describe('build', function() {
-	it('should built', function(done) {
-		opm.getPackage(path.join(__dirname, 'workspace/abc'), function(err, package) {
-			package.buildFile('abc.js', function(err, content) {
-				console.log(content, err);
-				done();
-			});
+describe('typed package', function() {
+	it('default package', function(done) {
+		opm.getPackage('test/workspace/abc', function(err, package) {
+			console.log(package.id);
+			done();
 		});
 	});
+	it('component package', function(done) {
+		opm.getPackage('test/workspace/brix', function(err, package) {
+			console.log(package.id);
+			done();
+		});
+	});
+	it('project package', function(done) {
+		opm.getPackage('test/workspace/brix/components/abc', function(err, package) {
+			console.log(package.id);
+			done();
+		});
+	});
+});
+
+describe('build', function() {
+	// it('should built', function(done) {
+	// 	opm.getPackage(path.join(__dirname, 'workspace/abc'), function(err, package) {
+	// 		package.buildFile('abc.js', function(err, content) {
+	// 			done();
+	// 		});
+	// 	});
+	// });
 });
